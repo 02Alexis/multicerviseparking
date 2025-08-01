1de18dd6773ef278be3fabef08e72aba3aeaf531
02Alexis <m_chael1012@hotmail.com>
2025-08-01 11:00:37 -0500

    solución de errores del deployd

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -o errexit
 
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-
-python manage.py migrate
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
--- multi_service_parking/wsgi.py
+++ multi_service_parking/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'multi_service_parking.settings')
 
 application = get_wsgi_application()
+
+app = application
